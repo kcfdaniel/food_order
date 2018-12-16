c12891034f9eef071b4d45f38c9cd4f3de1aca48
kcfdaniel <kcfdaniel@yahoo.com.hk>
2018-12-16 17:57:52 +0800

    fixed NavTabs glitching bugs by migrating: MaterializeCSS to Material UI

--- src/components/layout/NavTabs.js
+++ src/components/layout/NavTabs.js
@@ -1,15 +1,22 @@
 import React from 'react'
-//react materialize implementation
-import {Tabs, Tab} from 'react-materialize'
+//(old) react materialize implementation
+// import {Tabs, Tab} from 'react-materialize'
 
 // material ui implementation
-// import {Tabs, Tab} from '@material-ui/core';
-// import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import {Tabs, Tab} from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import {Home, Photo, RestaurantMenu, Person} from '@material-ui/icons';
 
+
+//material ui
 const styles = theme => ({
     root: {
       flexGrow: 1,
-      backgroundColor: theme.palette.background.paper,
+      backgroundColor: 'transparent',
+    },
+    indicator: {
+        backgroundColor: 'black',
     },
 });
   
@@ -19,52 +26,110 @@ class NavTabs extends React.Component {
     };
     handleChange = (event, value) => {
         this.setState({ value });
+        const {history} = this.props;
+        switch (value){
+            case 0:
+                history.push('/');
+                return
+            case 1:
+                history.push('/photos');
+                return
+            case 2:
+                history.push('/lunch_menu');
+                return
+            case 3:
+                history.push('/profile');
+                return
+            default:
+                console.log("default case: value is" + value)
+                return
+        }
     };
 
+    componentDidMount(){
+        const {history} = this.props;
+        let value = 0;
+        switch (history.location.pathname){
+            case "/":
+                value = 0;
+                break
+            case "/photos":
+                value = 1;
+                break
+            case "/lunch_menu":
+                value = 2;
+                break
+            case "/profile":
+                value = 3;
+                break
+            default:
+                console.log("default case: history.location.pathname is" + history.location.pathname);
+                history.push('/');
+                break
+        }
+        this.setState({
+            value
+        })
+    }
+
     render() {
-        const { history } = this.props;
+        const { history, classes } = this.props;
         const { value } = this.state;
         console.log("props:");
         console.log(this.props);
         return (
-            //react materialize implementation
-            <Tabs className='tab-demo tabs-transparent' onChange={(index) => {
-                switch (index.slice(-1)){
-                    case "0":
-                        history.push('/');
-                        return
-                    case "1":
-                        history.push('/photos');
-                        return
-                    case "2":
-                        history.push('/lunch_menu');
-                        return
-                    case "3":
-                        history.push('/profile');
-                        return
-                    default:
-                        console.log("default case: index is" + index)
-                        return
-                }
-            }}>
-                <Tab title={<i className="material-icons waves-effect">home</i>} active={history.location.pathname == '/'}/>
-                <Tab title={<i className="material-icons waves-effect">photo</i>} active={history.location.pathname == '/photos'}/>
-                <Tab title={<i className="material-icons waves-effect">restaurant_menu</i>} active={history.location.pathname == '/lunch_menu'}/>
-                <Tab title={<i className="material-icons waves-effect">person</i>} active={history.location.pathname == '/profile'}/>
-            </Tabs>
+            //(old) react materialize implementation
+            // <Tabs className='tab-demo tabs-transparent' onChange={(index) => {
+            //     switch (index.slice(-1)){
+            //         case "0":
+            //             history.push('/');
+            //             return
+            //         case "1":
+            //             history.push('/photos');
+            //             return
+            //         case "2":
+            //             history.push('/lunch_menu');
+            //             return
+            //         case "3":
+            //             history.push('/profile');
+            //             return
+            //         default:
+            //             console.log("default case: index is" + index)
+            //             return
+            //     }
+            // }}>
+            //     <Tab title={<i className="material-icons waves-effect">home</i>} active={history.location.pathname == '/'}/>
+            //     <Tab title={<i className="material-icons waves-effect">photo</i>} active={history.location.pathname == '/photos'}/>
+            //     <Tab title={<i className="material-icons waves-effect">restaurant_menu</i>} active={history.location.pathname == '/lunch_menu'}/>
+            //     <Tab title={<i className="material-icons waves-effect">person</i>} active={history.location.pathname == '/profile'}/>
+            // </Tabs>
 
             // material ui implementation
-            // <Tabs value={value} onChange={this.handleChange} fullWidth>
-            //     <Tab label="Item One" />
-            //     <Tab label="Item Two" />
-            //     <Tab label="Item Three" />
-            // </Tabs>
+            <Tabs 
+                value={value} 
+                onChange={this.handleChange} 
+                fullWidth
+                scrollable
+                scrollButtons="auto"
+                classes={{
+                    root: classes.root,
+                    indicator: classes.indicator,
+                }}
+            >
+                <Tab label={<Home className={classes.icon} />} />
+                <Tab label={<Photo className={classes.icon} />} />
+                <Tab label={<RestaurantMenu className={classes.icon} />} />
+                <Tab label={<Person className={classes.icon} />} />
+            </Tabs>
         )
     }
 }
 
-//react materialize
-export default NavTabs
+//(old) react materialize implementation
+// export default NavTabs
 
 //material ui
-// export default withStyles(styles)(NavTabs);
+NavTabs.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+export default withStyles(styles)(NavTabs);
