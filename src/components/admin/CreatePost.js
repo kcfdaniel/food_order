8dd0ffd5af628b46cfce54aef7cd2638718c5dda
kcfdaniel <kcfdaniel@yahoo.com.hk>
2018-12-25 10:43:34 +0800

    bug fix

--- src/components/admin/CreatePost.js
+++ src/components/admin/CreatePost.js
@@ -81,7 +81,7 @@ class CreatePost extends Component {
     if ((this.state.videoURL == null || this.state.videoURL == "") && (this.state.picURL == null || this.state.picURL == "")) {
       error = "You have to select either a video or an image!"
     }
-    if ((this.state.videoURL != null || this.state.videoURL != "") && (this.state.picURL != null || this.state.picURL != "")) {
+    if ((this.state.videoURL != null && this.state.videoURL != "") && (this.state.picURL != null && this.state.picURL != "")) {
       error = "You can only select a video or an image!"
     }
     if (this.state.title == "") {
